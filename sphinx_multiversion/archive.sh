918c3deaee8fba28f97859a1e48fbcec89a08dc7
Ryan Li <ryali@nvidia.com>
2025-11-21 13:10:49 -0800

    Fix archive to not checkout

--- sphinx_multiversion/archive.sh
+++ sphinx_multiversion/archive.sh
@@ -6,13 +6,6 @@ TOP_FILE=$2
 REPO_DIR="$(git rev-parse --show-toplevel)"
 TOP_PREFIX=$(basename "$REPO_DIR")
 
-# Save current commit hash
-ORIGINAL_COMMIT=$(git rev-parse HEAD)
-
-# Make sure submodules point to the right commit for the target commit
-git checkout "$TARGET_COMMIT"
-git submodule update --init --recursive
-
 # Archive the main repo at the desired commit
 git archive --format=tar --output="$TOP_FILE" "$TARGET_COMMIT"
 
@@ -20,14 +13,13 @@ export TOP_PREFIX
 export TOP_FILE
 export TARGET_COMMIT
 
-git submodule foreach --recursive '
-  SUB_PREFIX="$displaypath/"
-  SUB_FILE="$(basename $(git rev-parse --show-toplevel))-$TARGET_COMMIT.tar"
-  git archive --format=tar --prefix="$SUB_PREFIX" --output="$SUB_FILE" HEAD
-  tar --concatenate --file="$TOP_FILE" "$SUB_FILE"
-  rm "$SUB_FILE"
-'
-
-# Return to the original commit
-git checkout "$ORIGINAL_COMMIT"
-git submodule update --init --recursive
+git config --file .gitmodules --get-regexp path | awk '{ print $2 }' | while read submodule_path; do
+  # Get the submodule commit referenced by the parent repo at $TARGET_COMMIT
+  SUB_COMMIT=$(git ls-tree $TARGET_COMMIT "$submodule_path" | awk '{print $3}')
+  SUB_PREFIX="$submodule_path/"
+  SUB_FILE="$(basename "$submodule_path")-$SUB_COMMIT.tar"
+  echo "Archiving submodule $submodule_path to $SUB_FILE"
+  git -C "$submodule_path" archive --format=tar --prefix="$SUB_PREFIX" --output="$SUB_FILE" "$SUB_COMMIT"
+  tar --concatenate --file="$TOP_FILE" "external/$SUB_FILE"
+  rm "external/$SUB_FILE"
+done
